454d97b3e8f290880dc1711dc5e016d7a367fd18
Eneko96 <imr.aem2526@gmail.com>
2022-03-15 22:48:52 +0100

    updats

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const $ = <T>(selector:any, scope = document): T => scope.querySelector(selector)
+const $js:HTMLTextAreaElement = $('#js')
+const $css:HTMLTextAreaElement = $('#css')
+const $html:HTMLTextAreaElement = $('#html')
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const createHtml = () => {
+  const html = $html.value
+  const css = $css.value
+  const js = $js.value
+
+  return `
+  <!DOCTYPE html>
+  <html lang="el">
+    <head>
+      <style>
+        ${css}
+      </style>
+    </head>
+  <script>
+    ${js}
+  </script>
+  <body>
+    ${html}
+  </body>
+  `
+}
+
+const update = () => {
+  const html = createHtml()
+  $<HTMLIFrameElement>('iframe').setAttribute('srcdoc', html)
+}
+
+$js.addEventListener('input', update)
+$css.addEventListener('input', update)
+$html.addEventListener('input', update)
